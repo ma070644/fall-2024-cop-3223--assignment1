f80a4355a0528b3def39bfe32af512e064b4e175
ma070644 <dehaltmatthew@gmail.com>
2024-09-09 02:34:13 +0000

    These are changes I have made

---
+++ fracturing.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+// Function prototypes
+double calculateDistance();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth();
+double calculateHeight();
+
+// Purpose: calls all of the functions 
+
+// Output: none
+
+//Precondition: none
+
+//Postcondition: uses the functions and displays the inputs made by the user.
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+
+    return 0;
+}
+
+
+// Purpose: calculates the distance between the two points.
+
+// Output: prints the two points and the distance between them. 
+
+//Precondition: the user is asked to enter values for the points.
+
+//Postcondition: returns the distance between those points.
+
+double calculateDistance() {
+     double x1, y1, x2, y2;
+
+    printf("Enter coordinates for point #1:\n");
+    scanf("%lf %lf", &x1, &y1);
+    
+    printf("Enter coordinates for point #2:\n");
+    scanf("%lf %lf", &x2, &y2);
+
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The distance between the two points is %.2f\n", distance);
+
+    return distance;
+}
+
+
+// Purpose: calculates the perimeter of a using the diameter, distance.
+
+// Output: prints the two points and the perimeter.
+
+//Precondition: calls the calculateDistance function to get the diameter.
+
+//Postcondition: returns a difficulty rating.
+
+double calculatePerimeter() {
+    double distance = calculateDistance();
+    double perimeter = PI * distance;
+
+    printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
+
+    return 3.0;
+}
+
+
+// Purpose: calculates the area of a circle using the diameter, distance.
+
+// Output: prints the two points and the area.
+
+//Precondition: calls the calculateDistance function to get the diameter.
+
+//Postcondition: returns a difficulty rating.
+
+double calculateArea() {
+    double distance = calculateDistance();
+    double radius = distance / 2;
+    double area = PI * radius * radius;
+
+    printf("The area of the city encompassed by your request is %.2f\n", area);
+
+    return 3.0;
+}
+
+
+// Purpose: calculates the width which is equivalent to the diameter, distance.
+
+// Output: prints the two points and the width.
+
+//Precondition: calls the calculateDistance function to get the diameter.
+
+//Postcondition: returns a difficulty rating.
+
+double calculateWidth() {
+     double x1, y1, x2, y2;
+
+    printf("Enter coordinates for point #1:\n");
+    scanf("%lf %lf", &x1, &y1);
+    
+    printf("Enter coordinates for point #2:\n");
+    scanf("%lf %lf", &x2, &y2);
+
+    double width = fabs(x2 - x1);
+
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The width of the city encompassed by your request is %.2f\n", width);
+
+    return 2.0;
+}
+
+
+// Purpose: calculates the height which is equivalent to the diameter, distance.
+
+// Output: prints the point and the height.
+
+//Precondition: calls the calculateDistance function to get the diameter.
+
+//Postcondition: returns a difficulty rating.
+
+double calculateHeight() {
+     double x1, y1, x2, y2;
+
+    printf("Enter coordinates for point #1:\n");
+    scanf("%lf %lf", &x1, &y1);
+    
+    printf("Enter coordinates for point #2:\n");
+    scanf("%lf %lf", &x2, &y2);
+
+    double height = fabs(y2 - y1);
+
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The height of the city encompassed by your request is %.2f\n", height);
+
+    return 2.0;
+}
